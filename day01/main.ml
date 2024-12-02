08bb56b92833e195942197c7cc6b3d38067339a3
Sri Aspari <mail@siarie.me>
2024-12-02 16:14:15 +0700

    update

--- day01/main.ml
+++ day01/main.ml
@@ -5,7 +5,6 @@ let split s =
   | [x;y] -> (int_of_string x, int_of_string y)
   | _ -> failwith "Error: input"
 
-
 let parse_location lst =
   let rec fn lst left right =
     match lst with
@@ -19,19 +18,15 @@ let parse_location lst =
 let calculate_distance left right =
   let sorted_left = List.sort (fun a b -> a - b) left in
   let sorted_right = List.sort (fun a b -> a - b) right in
-  let distances = ref [] in
+  let result = ref 0 in
   for i = 0 to List.length sorted_left - 1 do
     let distance = (List.nth sorted_left i) - (List.nth sorted_right i) in
-    distances := Int.abs distance :: !distances
+    result := !result + Int.abs distance
   done;
-  distances
+  result
 
 let () =
   let input = In_channel.with_open_text "day01/input.txt" In_channel.input_lines in
   let (left,right) = parse_location input in
-  let distances = calculate_distance left right in
-  let result = ref 0 in
-  for i = 0 to List.length !distances - 1 do
-    result := !result + List.nth !distances i
-  done;
-  print_endline (string_of_int !result)
+  let distance = calculate_distance left right in
+  print_endline (string_of_int !distance)
