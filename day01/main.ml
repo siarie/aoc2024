e0114f5e8565a4739584f9c61d3966c307b7e4ca
Sri Aspari <mail@siarie.me>
2024-12-02 16:06:27 +0700

    aoc: day 1

---
+++ day01/main.ml
@@ -0,0 +1,37 @@
+let split s =
+  let parts = String.split_on_char ' ' s in
+  let filter = List.filter (fun s -> s <> "") parts in
+  match filter with
+  | [x;y] -> (int_of_string x, int_of_string y)
+  | _ -> failwith "Error: input"
+
+
+let parse_location lst =
+  let rec fn lst left right =
+    match lst with
+    | [] -> (left,right)
+    | h::t ->
+       let (a,b) = split h in
+       fn t (a::left) (b::right)
+  in
+  fn lst [] []
+
+let calculate_distance left right =
+  let sorted_left = List.sort (fun a b -> a - b) left in
+  let sorted_right = List.sort (fun a b -> a - b) right in
+  let distances = ref [] in
+  for i = 0 to List.length sorted_left - 1 do
+    let distance = (List.nth sorted_left i) - (List.nth sorted_right i) in
+    distances := Int.abs distance :: !distances
+  done;
+  distances
+
+let () =
+  let input = In_channel.with_open_text "day01/input.txt" In_channel.input_lines in
+  let (left,right) = parse_location input in
+  let distances = calculate_distance left right in
+  let result = ref 0 in
+  for i = 0 to List.length !distances - 1 do
+    result := !result + List.nth !distances i
+  done;
+  print_endline (string_of_int !result)
